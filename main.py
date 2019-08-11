5d65cb8dc499f51a0a8c1d7d23cf859581326239
hermanda <daniel.herman19@gmail.com>
2019-08-11 13:38:12 +0200

    Added: opening files funct, centering img

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication, QPushButton, QLineEdit
-from PySide2.QtCore import QFile, QObject
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QFileDialog, QApplication, QSizePolicy
+from PyQt5.QtGui import QIcon
 
 class Pane(QtWidgets.QScrollArea):
     MinWidth = 186
@@ -42,16 +41,43 @@ class Pane(QtWidgets.QScrollArea):
                 self.setFixedWidth(Pane.MinWidth)
         return super(Pane, self).eventFilter(source, event)
 
-class Main(QtWidgets.QMainWindow):
-
+class Main(QMainWindow):
     def __init__(self, parent=None):
         super(Main, self).__init__(parent)
+        
+        self.files = [];
 
+        self.initUI()
+      
+    def initUI(self):
         uic.loadUi('mainwindow_simple.ui', self)
         self.MidColumn = self.findChild(QtWidgets.QFrame, 'MidColumn')
         self.RightColumn = self.findChild(QtWidgets.QFrame, 'RightColumn')
         self.LabelAddButton = self.findChild(QtWidgets.QPushButton, 'LabelAddButton')
         self.LabelAddButton.clicked.connect(self.LabelAdd) 
+
+        # Menu setup
+        self.actionOpen_files.setShortcut("Ctrl+O")
+        self.actionOpen_files.triggered.connect(self.files_open)
+
+        # Frame for a picture setup
+        self.photo = QtWidgets.QLabel(self.MidColumn)
+        # self.horizontalLayout_mid = QtWidgets.QHBoxLayout(self.MidColumn)
+        # self.horizontalLayout_mid.addWidget(self.photo)
+        
+        self.photo.setText("")
+        myPixmap = QtGui.QPixmap('apple.jpg')
+        myPixmap.scaled(self.photo.size(), QtCore.Qt.KeepAspectRatio, QtCore.Qt.FastTransformation)
+        #self.photo.setScaledContents(True)
+        
+        self.photo.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        
+        self.photo.setPixmap(myPixmap)
+        # self.photo.setAlignment(QtCore.Qt.AlignCenter)
+        # self.MidColumn.setAlignment( QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter );
+        vector = self.MidColumn.rect().center() - QtCore.QRect(QtCore.QPoint(), self.photo.sizeHint()).center()
+        self.photo.move(vector)
+        self.photo.setObjectName("photo")
 
         self.show()
 
@@ -62,9 +88,10 @@ class Main(QtWidgets.QMainWindow):
         language = 'None' if not self.line.text() else self.line.text()
         print('Favorite language: {}'.format(language))
 
-    def file_open(self):
-        name = QtGui.QFileDialog.getOpenFileNames(self, 'Open File')
-        file = open(name,'r')
+    def files_open(self):
+        names = QFileDialog.getOpenFileNames(self, 'Open File', ("Images (*.png *.xpm *.jpg)"))
+        print(names)
+        file = open(names[0],'r')
 
         self.editor()
 
